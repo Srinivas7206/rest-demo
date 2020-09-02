774ab1cf3ba58e3f231d6e237054b603f47e50df
Karuth <karuthtechnologies@gmail.com>
2020-09-02 12:54:44 +0530

    First commit

---
+++ src/main/java/com/karuth/restdemo/contorllers/TestController.java
@@ -0,0 +1,18 @@
+package com.karuth.restdemo.contorllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+	@GetMapping("/message")
+	public ResponseEntity<String>getMessage(){
+		String message = "Hello, World";
+		return new ResponseEntity<String>(message,HttpStatus.OK);
+	}
+}
